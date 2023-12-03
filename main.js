cbeb7236eeabc55769282704f09458367f810dc8
iqbolkhuja2009 <142048200+iqbolkhuja2009@users.noreply.github.com>
2023-12-03 18:00:20 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// 1-masala// let n = +prompt("Sonni kiriting");
+// let sum = 0;
+// for(let i = 1; i <= n; i++){
+//     if(i % 2 == 1){
+//         sum += i;
+//     }
+// }
+// 2-masala
+let n = +prompt("Sonni kiriting");
+let sum = 1;
+for (let i = n; i >= 1; i--);
+if (i % 3 == 0){
+    if(i % 3 == 0){
+        sum *= i; 
+    }
+   }  
+   console.log(sum);
