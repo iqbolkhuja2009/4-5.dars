e9dfed9abcb9f4f719b28d42d0c868e274b0754b
iqbolkhuja2009 <142048200+iqbolkhuja2009@users.noreply.github.com>
2023-12-03 18:02:00 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,92 @@
-// 1-masala// let n = +prompt("Sonni kiriting");
-// let sum = 0;
-// for(let i = 1; i <= n; i++){
-//     if(i % 2 == 1){
-//         sum += i;
-//     }
+// 1-masala
+// let n = 10; 
+// let yigindi = 0;
+// for (let i = 1; i <= n; i += 2) {
+//   yigindi += i;
 // }
-// 2-masala
-let n = +prompt("Sonni kiriting");
-let sum = 1;
-for (let i = n; i >= 1; i--);
-if (i % 3 == 0){
-    if(i % 3 == 0){
-        sum *= i; 
-    }
-   }  
-   console.log(sum);
+// console.log(yigindi); 
+//2-masala 
+//let n = 10; 
+// let kopaytma = 1;
+// for (let i = n; i >= 1; i -= 3) {
+//   kopaytma *= i;
+// }
+// console.log(kopaytma);
+// 3-masala 
+// let a = 1; 
+// let b = 10; 
+// let kopaytma = 1;
+// for (let i = a; i <= b; i += 3) {
+//   kopaytma *= i;
+// }
+// console.log(kopaytma); 
+// 4-masala
+// let a = 1;
+// let b = 10; 
+// let yigindi = 0;
+// for (let i = a; i <= b; i++) {
+//   if (i % 5 === 0 || i % 7 === 0) {
+//     yigindi += i;
+//   }
+// }
+// console.log(yigindi);
+// 5-masala 
+// let a = 1; 
+// let b = 10;
+// let yigindi = 0;
+// for (let i = a; i <= b; i++) {
+//   if (i % 2 !== 0) {
+//     yigindi += i * i; 
+//   }
+// }
+// console.log(yigindi); 
+// 6-masala
+// let a = 1; 
+// let b = 10; 
+// let kopaytma = 1;
+// for (let i = a; i <= b; i++) {
+//   if (i % 2 === 0) {
+//     kopaytma *= i * i * i; 
+//   }
+// }
+// console.log(kopaytma); 
+// 7-masala
+// let son = 12; 
+// console.log(`${son} ning bo'luvchilari:`);
+// for (let i = 1; i <= son; i++) {
+//   if (son % i === 0) {
+//     console.log(i);
+//   }
+// }
+// 8-masala
+// let son = 20;
+// let boluvchilar = [];
+// console.log(`${son} ning boluvchilari:`);
+// for (let i = 1; i <= son; i++) {
+//   if (son % i === 0) {
+//     boluvchilar.push(i);
+//   }
+// }
+// console.log(boluvchilar);
+// 9-masala
+// let son = 17; 
+// let tubmi = true;
+// if (son <= 1) {
+//   tubmi = false;
+// } else {
+//   for (let i = 2; i <= Math.sqrt(son); i++) {
+//     if (son % i === 0) {
+//       tubmi = false;
+//       break;
+//     }
+//   }
+// }
+// if (tubmi) {
+//   console.log(`${son} tub son.`);
+// } else {
+//   console.log(`${son} tub emas son.`);
+// }
+
+
+
+
